2339066e748b920ff264a1965de32e9428ef04e2
dhun <dhun.hosoya@gmail.com>
2013-03-08 21:14:41 +0900

    warning delete

--- application/src/main/java/jp/gr/java_conf/dhun/starseeker/model/Coordinates.java
+++ application/src/main/java/jp/gr/java_conf/dhun/starseeker/model/Coordinates.java
@@ -12,6 +12,6 @@ package jp.gr.java_conf.dhun.starseeker.model;
 @Deprecated
 public class Coordinates {
 
-    private double longitude; // 経度(λ). 東経を - 西経を + とする. -180から+180
-    private double latitude;  // 緯度(ψ). 北緯を + 南緯を - とする. +90から-90
+    public double longitude; // 経度(λ). 東経を - 西経を + とする. -180から+180
+    public double latitude;  // 緯度(ψ). 北緯を + 南緯を - とする. +90から-90
 }
