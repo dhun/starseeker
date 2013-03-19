a4d6504f2084b215763d66debf0982b005801a64
dhun <dhun.hosoya@gmail.com>
2013-03-19 21:50:55 +0900

    星インスタンスを追加

--- application/src/main/java/jp/gr/java_conf/dhun/starseeker/model/EquatorialCoordinateSystem.java
+++ application/src/main/java/jp/gr/java_conf/dhun/starseeker/model/EquatorialCoordinateSystem.java
@@ -16,29 +16,29 @@ public class EquatorialCoordinateSystem {
 
     static {
         STARS = new HashSet<Star>();
-        STARS.add(newStar("ポラリス", "こぐま", "02h 31.8m", "+89°16'", "ご存知北極星。赤緯９０°ではありません。"));
-        // STARS.add(newStar("アルデバラン", "おうし", "04h 35m", "9°+16 31'"));
-        // STARS.add(newStar("リゲル", "オリオン", "05h 14.5m", "-08°12'"));
-        // STARS.add(newStar("カペラ", "ぎょしゃ", "05h 16.7m", "+46°00'"));
-        // STARS.add(newStar("ベテルギウス", "オリオン", "05h 55.2m", "+07°24'"));
-        // STARS.add(newStar("カノープス", "りゅうこつ", "06h 24.0m", "-52°42'"));
-        // STARS.add(newStar("シリウス", "おおいぬ", "06h 45.1m", "-16°43'"));
-        // STARS.add(newStar("プロキオン", "こいぬ", "07h 39.3m", "+05°14'"));
-        // STARS.add(newStar("レグルス", "わし", "10h 08.4m", "+11°58'"));
-        // STARS.add(newStar("スピカ", "おとめ", "13h 25.2m", "-11°10'"));
-        // STARS.add(newStar("アンタレス", "さそり", "16h 29.4m", "-26°26'"));
-        // STARS.add(newStar("べガ", "こと", "18h 36.9m", "+38°47'"));
-        // STARS.add(newStar("デネブ", "はくちょう", "20h 41.4m", "+45°17'"));
+        STARS.add(newStar("02h 31.8m", "+89°16'", "ポラリス", "こぐま", "ご存知北極星。赤緯９０°ではありません。"));
+        STARS.add(newStar("04h 35.0m", "+09°31'", "アルデバラン", "おうし")); // XXX 値を確認する
+        STARS.add(newStar("05h 14.5m", "-08°12'", "リゲル", "オリオン"));
+        STARS.add(newStar("05h 16.7m", "+46°00'", "カペラ", "ぎょしゃ"));
+        STARS.add(newStar("05h 55.2m", "+07°24'", "ベテルギウス", "オリオン"));
+        STARS.add(newStar("06h 24.0m", "-52°42'", "カノープス", "りゅうこつ"));
+        STARS.add(newStar("06h 45.1m", "-16°43'", "シリウス", "おおいぬ"));
+        STARS.add(newStar("07h 39.3m", "+05°14'", "プロキオン", "こいぬ"));
+        STARS.add(newStar("10h 08.4m", "+11°58'", "レグルス", "わし"));
+        STARS.add(newStar("13h 25.2m", "-11°10'", "スピカ", "おとめ"));
+        STARS.add(newStar("16h 29.4m", "-26°26'", "アンタレス", "さそり"));
+        STARS.add(newStar("18h 36.9m", "+38°47'", "べガ", "こと"));
+        STARS.add(newStar("20h 41.4m", "+45°17'", "デネブ", "はくちょう"));
     }
 
-    private static Star newStar(String name, String starSignName, String rightAscension, String declination, String memo) {
-        return newStar(name, starSignName, rightAscension, declination);
-
-    }
-
-    private static Star newStar(String name, String starSignName, String rightAscension, String declination) {
+    private static Star newStar(String rightAscension, String declination, String name, String starSignName, String memo) {
         Star star = new Star(rightAscension, declination);
         star.setName(name);
+        // star.setMemo(memo); // XXX 後で実装する
         return star;
+    }
+
+    private static Star newStar(String rightAscension, String declination, String name, String starSignName) {
+        return newStar(rightAscension, declination, name, starSignName, null);
     }
 }
